afa4186fdd23b91aeca8cac86887e532fc09d815
Loïc Niederhauser <loic.niederhauser@gmail.com>
2019-04-04 13:32:33 +0200

    Modified structure for new lab

---
+++ LAB06/Python/cmcpack/default.py
@@ -0,0 +1,10 @@
+""" Default parameters """
+import os
+
+DEFAULT = {
+    "label": ["State 0", "State 1"],
+    "save_figures": True,
+    "save_extensions": ["png"],
+    "save_folder": os.getcwd().replace("Python", "Figures")+"\\"
+}
+
