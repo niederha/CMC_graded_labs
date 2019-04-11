cd98ceeb764e1183311165ef99dba7211d412a05
Loïc Niederhauser <loic.niederhauser@gmail.com>
2019-04-11 14:37:59 +0200

    exercise 3 wrapup

--- LAB06/Python/exercise3.py
+++ LAB06/Python/exercise3.py
@@ -7,6 +7,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 import cmc_pylog as pylog
+from time_parameters import TimeParameters
 from cmcpack import DEFAULT
 from cmcpack.plot import save_figure
 from muscle import Muscle
@@ -29,146 +30,154 @@ plt.rc('xtick', labelsize=14.0)    # fontsize of the tick labels
 plt.rc('ytick', labelsize=14.0)    # fontsize of the tick labels
 
 
-def exercise3():
-    """ Main function to run for Exercise 3.
+def plot_results(res, prefix):
+    """ PLots every important graph for a simulation an labels them using the prefix"""
 
-    Parameters
-    ----------
-        None
+    activation_handle = prefix+'_activations'
+    state_handle = prefix+'_state'
+    phase_handle = prefix+'_phase'
 
-    Returns
-    -------
-        None
-    """
-    # Define and Setup your pendulum model here
-    # Check Pendulum.py for more details on Pendulum class
-    P_params = PendulumParameters()  # Instantiate pendulum parameters
-    P_params.L = 0.5  # To change the default length of the pendulum
-    P_params.m = 1.  # To change the default mass of the pendulum
-    pendulum = PendulumSystem(P_params)  # Instantiate Pendulum object
+    # Activations
+    plt.figure(activation_handle)
+    plt.title('Oscillating activations through time')
+    plt.plot(res[:, 0], res[:, 3], label="Activation 1")
+    plt.plot(res[:, 0], res[:, 5], label="Activation 2")
+    plt.xlabel('Time [s]')
+    plt.ylabel('Activation intensity')
+    plt.legend()
+    plt.grid()
 
-    #### CHECK OUT Pendulum.py to ADD PERTURBATIONS TO THE MODEL #####
+    # Plotting the results
+    plt.figure(state_handle)
+    plt.title('State of the pendulum')
+    plt.plot(res[:, 0], res[:, 1])
+    plt.xlabel('Time [s]')
+    plt.ylabel(r'$\theta$[rad]')
+    plt.grid()
 
-    pylog.info('Pendulum model initialized \n {}'.format(
-        pendulum.parameters.showParameters()))
+    # Plotting the results
+    plt.figure(phase_handle)
+    plt.title('Phase of the pendulum')
+    plt.plot(res[:, 1], res[:, 2])
+    plt.xlabel(r'$\theta$[rad]')
+    plt.ylabel(r'$\dot{\theta}$[rad]')
+    plt.grid()
+    return activation_handle, state_handle, phase_handle
 
-    # Define and Setup your pendulum model here
-    # Check MuscleSytem.py for more details on MuscleSytem class
-    M1_param = MuscleParameters()  # Instantiate Muscle 1 parameters
-    M1_param.f_max = 1500  # To change Muscle 1 max force
-    M2_param = MuscleParameters()  # Instantiate Muscle 2 parameters
-    M2_param.f_max = 1500  # To change Muscle 2 max force
-    M1 = Muscle(M1_param)  # Instantiate Muscle 1 object
-    M2 = Muscle(M2_param)  # Instantiate Muscle 2 object
-    # Use the MuscleSystem Class to define your muscles in the system
-    muscles = MuscleSytem(M1, M2)  # Instantiate Muscle System with two muscles
-    pylog.info('Muscle system initialized \n {} \n {}'.format(
-        M1.parameters.showParameters(),
-        M2.parameters.showParameters()))
+def system_initialisation(l_pendulum=0.5, m_pendulum=1., f_max=1500, l_attach=0.17):
+    """Generates a oscillatory system and its default initial conditions"""
 
-    # Define Muscle Attachment points
-    m1_origin = np.array([-0.17, 0.0])  # Origin of Muscle 1
-    m1_insertion = np.array([0.0, -0.17])  # Insertion of Muscle 1
+    # Neural parameters for oscillatory system
+    d = 1.
+    w = np.array([[0, -5, -5, 0],
+                           [-5, 0, 0, -5],
+                           [5, -5, 0, 0],
+                           [-5, 5, 0, 0]])
+    b = np.array([3., 3., -3., -3.])
+    tau = np.array([0.02, 0.02, 0.1, 0.1])
 
-    m2_origin = np.array([0.17, 0.0])  # Origin of Muscle 2
-    m2_insertion = np.array([0.0, -0.17])  # Insertion of Muscle 2
+    # Pendulum parameters
+    pendulum_params = PendulumParameters()
+    pendulum_params.L = l_pendulum
+    pendulum_params.m = m_pendulum
+    pendulum = PendulumSystem(pendulum_params)
 
-    # Attach the muscles
+    # Muscles parameters
+    m1_param = MuscleParameters()
+    m1_param.f_max = f_max
+    m2_param = MuscleParameters()
+    m2_param.f_max = f_max
+    m1 = Muscle(m1_param)
+    m2 = Muscle(m2_param)
+    muscles = MuscleSytem(m1, m2)
+
+    # Muscle_attachment
+    m1_origin = np.array([-l_attach, 0.0])
+    m1_insertion = np.array([0.0, -l_attach])
+    m2_origin = np.array([l_attach, 0.0])
+    m2_insertion = np.array([0.0, -l_attach])
     muscles.attach(np.array([m1_origin, m1_insertion]),
                    np.array([m2_origin, m2_insertion]))
 
-    ##### Neural Network #####
-    # The network consists of four neurons
-    N_params = NetworkParameters()  # Instantiate default network parameters
-    N_params.D = 2.  # To change a network parameter
-    # Similarly to change w -> N_params.w = (4x4) array
+    # Neural network
+    n_params = NetworkParameters()
+    n_params.D = d
+    n_params.w = w
+    n_params.b = b
+    n_params.tau = tau
+    neural_network = NeuralSystem(n_params)
 
-    # Create a new neural network with above parameters
-    neural_network = NeuralSystem(N_params)
-    pylog.info('Neural system initialized \n {}'.format(
-        N_params.showParameters()))
+    # System creation
+    sys = System()                         # Instantiate a new system
+    sys.add_pendulum_system(pendulum)      # Add the pendulum model to the system
+    sys.add_muscle_system(muscles)         # Add the muscle model to the system
+    sys.add_neural_system(neural_network)  # Add neural network model to the system
 
-    # Create system of Pendulum, Muscles and neural network using SystemClass
-    # Check System.py for more details on System class
-    sys = System()  # Instantiate a new system
-    sys.add_pendulum_system(pendulum)  # Add the pendulum model to the system
-    sys.add_muscle_system(muscles)  # Add the muscle model to the system
-    # Add the neural network to the system
-    sys.add_neural_system(neural_network)
+    # Default initial conditions
+    x0_p = np.array([np.pi / 4, 0.])                # Pendulum initial condition
+    x0_m = np.array([0., m1.L_OPT, 0., m2.L_OPT])   # Muscle Model initial condition
+    x0_n = np.array([-0.5, 1, 0.5, 1])              # Neural Network initial condition
+    x0 = np.concatenate((x0_p, x0_m, x0_n))         # System initial conditions
+    return sys, x0
 
-    ##### Time #####
-    t_max = 2.5  # Maximum simulation time
-    time = np.arange(0., t_max, 0.001)  # Time vector
 
-    ##### Model Initial Conditions #####
-    x0_P = np.array([0., 0.])  # Pendulum initial condition
+def exercise3a(time_param):
+    """ Instantiate simulates and plots a system in driven by an oscillatory neural network"""
 
-    # Muscle Model initial condition
-    x0_M = np.array([0., M1.L_OPT, 0., M2.L_OPT])
-
-    x0_N = np.array([-0.5, 1, 0.5, 1])  # Neural Network Initial Conditions
-
-    x0 = np.concatenate((x0_P, x0_M, x0_N))  # System initial conditions
-
-    ##### System Simulation #####
-    # For more details on System Simulation check SystemSimulation.py
-    # SystemSimulation is used to initialize the system and integrate
-    # over time
-
-    sim = SystemSimulation(sys)  # Instantiate Simulation object
-
-    # Add external inputs to neural network
-
-    # sim.add_external_inputs_to_network(np.ones((len(time), 4)))
-    # sim.add_external_inputs_to_network(ext_in)
-
-    sim.initalize_system(x0, time)  # Initialize the system state
-
-    # Integrate the system for the above initialized state and time
+    # Simulation
+    sys, x0 = system_initialisation()
+    sim = SystemSimulation(sys)
+    sim.initalize_system(x0, time_param.times)
     sim.simulate()
+    res = sim.results()  # [time, states]
 
-    # Obtain the states of the system after integration
-    # res is np.array [time, states]
-    # states vector is in the same order as x0
-    res = sim.results()
+    # Plots
+    plot_results(res, '3_a')
 
-    # Obtain the states of the system after integration
-    # res is np.array [time, states]
-    # states vector is in the same order as x0
-    res = sim.results()
 
-    # In order to obtain internal states of the muscle
-    # you can access the results attribute in the muscle class
-    muscle1_results = sim.sys.muscle_sys.Muscle1.results
-    muscle2_results = sim.sys.muscle_sys.Muscle2.results
+def exercise3b(time_param):
+    """ Simulate the system multiple times by applying various external drives and plots the results"""
+    drive_min = 0
+    drive_max = 1
+    nb_drives = 3
+    drives = np.linspace(drive_min, drive_max, nb_drives)
 
-    # Plotting the results
-    plt.figure('Pendulum')
-    plt.title('Pendulum Phase')
-    plt.plot(res[:, 0], res[:, :2])
-    plt.xlabel('Position [rad]')
-    plt.ylabel('Velocity [rad.s]')
-    plt.grid()
+    for drive in drives:
+        # Simulate for drive
+        sys, x0 = system_initialisation()
+        sim = SystemSimulation(sys)
+        sim.initalize_system(x0, time_param.times)
+        ext_drive = np.ones((time_param.nb_pts, 4))*drive
+        ext_drive[0:int(time_param.nb_pts/2), :] *= 0
+        sim.add_external_inputs_to_network()
+        sim.simulate()
+        res = sim.results()
 
-    if DEFAULT["save_figures"] is False:
-        plt.show()
-    else:
+        # Plot
+        activation_handle, state_handle, phase_handle = plot_results(res, '3_b_'+str(drive))
+
+        # Add marker where external drive was applied
+        plt.figure(activation_handle)
+        plt.axvline(time_param.times[int(time_param.nb_pts/2)], color='k')
+        plt.text(time_param.times[int(time_param.nb_pts/2)]+0.1, 0.2, "External input", rotation=-90, color="k")
+        plt.figure(state_handle)
+        plt.axvline(time_param.times[int(time_param.nb_pts / 2)], color='k')
+        plt.text(time_param.times[int(time_param.nb_pts / 2)] + 0.1, 0.6, "External input", rotation=-90, color="k")
+
+
+def exercise3():
+
+    time_param = TimeParameters(time_start=0, time_stop=10., time_step=0.001)
+    # exercise3a(time_param)
+    exercise3b(time_param)
+
+    if DEFAULT["save_figures"]:
         figures = plt.get_figlabels()
         pylog.debug("Saving figures:\n{}".format(figures))
         for fig in figures:
             plt.figure(fig)
             save_figure(fig)
-            plt.close(fig)
-
-    # To animate the model, use the SystemAnimation class
-    # Pass the res(states) and systems you wish to animate
-    simulation = SystemAnimation(
-        res,
-        sim.sys.pendulum_sys,
-        sim.sys.muscle_sys,
-        sim.sys.neural_sys)
-    # To start the animation
-    simulation.animate()
+    plt.show()
 
 
 if __name__ == '__main__':
