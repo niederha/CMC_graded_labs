37befa61f600d25829ba019f899b0247d4e14108
Loïc Niederhauser <loic.niederhauser@gmail.com>
2019-04-11 15:59:20 +0200

    Updated names to match naming conventions on master

--- Lab5/Python/cmcpack/default.py
+++ Lab5/Python/cmcpack/default.py
@@ -5,6 +5,6 @@ DEFAULT = {
     "label": ["State 0", "State 1"],
     "save_figures": True,
     "save_extensions": ["png"],
-    "save_folder": os.getcwd().replace("Python", "Figures")+"\\"
+    "save_folder": os.getcwd().replace("Python", "figs")+"\\"
 }
 
