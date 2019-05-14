52f8b92faec86d44a8179480dd18c980e8f6e065
Loïc Niederhauser <loic.niederhauser@gmail.com>
2019-05-14 10:09:01 +0200

    Nicer plots

--- Lab9/Webots/controllers/pythonController/save_figures.py
+++ Lab9/Webots/controllers/pythonController/save_figures.py
@@ -6,18 +6,15 @@ import cmc_pylog as pylog
 
 def save_figure(figure, name=None, **kwargs):
     """ Save figure """
-    for extension in kwargs.pop("extensions", ["pdf"]):
+    for extension in kwargs.pop("extensions", ["png"]):
         fig = figure.replace(" ", "_").replace(".", "dot")
         if name is None:
             name = "{}.{}".format(fig, extension)
         else:
             name = "{}.{}".format(name, extension)
-        fig = plt.figure(figure)
-        size = plt.rcParams.get('figure.figsize')
-        fig.set_size_inches(0.7*size[0], 0.7*size[1], forward=True)
+        plt.figure(figure)
         plt.savefig(name, bbox_inches='tight')
         pylog.debug("Saving figure {}...".format(name))
-        fig.set_size_inches(size[0], size[1], forward=True)
 
 
 def save_figures(**kwargs):
@@ -25,5 +22,5 @@ def save_figures(**kwargs):
     figures = [str(figure) for figure in plt.get_figlabels()]
     pylog.debug("Other files:\n    - " + "\n    - ".join(figures))
     for name in figures:
-        save_figure(name, extensions=kwargs.pop("extensions", ["pdf"]))
+        save_figure(name, extensions=kwargs.pop("extensions", ["png"]))
 
