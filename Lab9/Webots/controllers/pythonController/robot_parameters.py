6778ea4d6e4087310adf3e9c0db3b9dddc328c7f
Loïc Niederhauser <loic.niederhauser@gmail.com>
2019-05-14 10:11:34 +0200

    Added mlr_drive, saturation, and neural_net param. update

--- Lab9/Webots/controllers/pythonController/robot_parameters.py
+++ Lab9/Webots/controllers/pythonController/robot_parameters.py
@@ -1,6 +1,7 @@
 """Robot parameters"""
 
 import numpy as np
+import neural_net_matrice_generation as nnmg
 import cmc_pylog as pylog
 
 
@@ -28,50 +29,71 @@ class RobotParameters(dict):
         self.phase_bias = np.zeros([self.n_oscillators, self.n_oscillators])
         self.rates = np.zeros(self.n_oscillators)
         self.nominal_amplitudes = np.zeros(self.n_oscillators)
+        self.mlr_drive = 1.
         self.update(parameters)
 
     def update(self, parameters):
         """Update network from parameters"""
         self.set_frequencies(parameters)  # f_i
         self.set_coupling_weights(parameters)  # w_ij
-        self.set_phase_bias(parameters)  # theta_i
+        self.set_phase_bias(parameters)  # phi_ij
         self.set_amplitudes_rate(parameters)  # a_i
         self.set_nominal_amplitudes(parameters)  # R_i
 
     def set_frequencies(self, parameters):
         """Set frequencies"""
-        pylog.warning("Coupling weights must be set")
+
+        # Unsaturated body
+        if parameters.body_saturation[0] <= parameters.mlr_drive <= parameters.body_saturation[1]:
+            self.freqs[:self.n_body_joints] = parameters.c_freq_body[0] + \
+                                              parameters.mlr_drive * parameters.c_freq_body[1]
+
+        # Saturated body
+        else:
+            self.freqs[:self.n_body_joints] = parameters.v_sat
+
+        # Unsaturated limbs
+        if parameters.limb_saturation[0] <= parameters.mlr_drive <= parameters.limb_saturation[1]:
+            self.freqs[self.n_body_joints:] = parameters.c_freq_limb[0] + \
+                                              parameters.mlr_drive * parameters.c_freq_limb[1]
+
+        # Saturated limbs
+        else:
+            self.freqs[self.n_body_joints:] = parameters.v_sat
 
     def set_coupling_weights(self, parameters):
         """Set coupling weights"""
-        weak_coupling = 10.
-        strong_coupling = 30.
-
-
-        # Couple body oscillators
-        for i in range(self.n_oscillators_body):
-            # Neighbour coupling
-            if i != (self.n_oscillators_body/2)-1:
-                self.coupling_weights[i][i+1] = weak_coupling
-                self.coupling_weights[i+1][i] = weak_coupling
-            # Anti-phase coupling
-            if i < self.n_oscillators_body:
-                self.coupling_weights[i][i+self.n_oscillators_body/2] = weak_coupling
-                self.coupling_weights[i + self.n_oscillators_body / 2][i] = weak_coupling
-
-        # Limb coupling
-        for i in range(self.n_oscillators_legs):
-
+        self.coupling_weights = np.genfromtxt(nnmg.coupling_weights_file, delimiter=',')
 
     def set_phase_bias(self, parameters):
         """Set phase bias"""
-        pylog.warning("Phase bias must be set")
+        self.phase_bias = np.genfromtxt(nnmg.phase_bias_file, delimiter=',')
 
     def set_amplitudes_rate(self, parameters):
         """Set amplitude rates"""
-        pylog.warning("Convergence rates must be set")
+        self.rates = np.full(self.n_oscillators, parameters.rates)
 
     def set_nominal_amplitudes(self, parameters):
         """Set nominal amplitudes"""
-        pylog.warning("Nominal amplitudes must be set")
+
+        # Unsaturated body
+        if parameters.body_saturation[0] <= parameters.mlr_drive <= parameters.body_saturation[1]:
+            self.nominal_amplitudes[:self.n_body_joints] = parameters.c_R_body[0] + \
+                                                           parameters.mlr_drive * parameters.c_R_body[1]
+
+        # Saturated body
+        else:
+            self.nominal_amplitudes[:self.n_body_joints] = parameters.R_sat
+
+        # Unsaturated limbs
+        if parameters.limb_saturation[0] <= parameters.mlr_drive <= parameters.limb_saturation[1]:
+            self.nominal_amplitudes[self.n_body_joints:] = parameters.c_R_limb[0] + \
+                                                           parameters.mlr_drive * parameters.c_R_limb[1]
+
+        # Saturated limbs
+        else:
+            self.nominal_amplitudes[self.n_body_joints:] = parameters.R_sat
+
+    def set_mlr_drive(self, parameters):
+        self.mlr_drive = parameters.mlr_drive
 
