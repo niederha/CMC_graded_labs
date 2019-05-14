46aa5ae25e9dfd15e3db078fdce0c38d196a883e
Loïc Niederhauser <loic.niederhauser@gmail.com>
2019-05-14 13:12:00 +0200

    Implementation to run grid search

--- Lab9/Webots/controllers/pythonController/exercise_9b.py
+++ Lab9/Webots/controllers/pythonController/exercise_9b.py
@@ -7,5 +7,26 @@ from simulation_parameters import SimulationParameters
 
 def exercise_9b(world, timestep, reset):
     """Exercise 9b"""
-    pass
+    # Parameters
+    n_joints = 10
+
+    parameter_set = [
+        SimulationParameters(
+            simulation_duration=30,
+            mlr_drive=5.,
+        )
+    ]
+
+    # Grid search
+    for simulation_i, parameters in enumerate(parameter_set):
+        reset.reset()
+        run_simulation(
+            world,
+            parameters,
+            timestep,
+            int(1000 * parameters.simulation_duration / timestep),
+            logs="./logs/9b_walking/simulation_{}.npz".format(simulation_i)
+        )
+
+
 
