2637892676d01f34a48a5ba312e40f70c0131a82
Loïc Niederhauser <loic.niederhauser@gmail.com>
2019-05-14 14:52:13 +0200

    Test walking on ground

--- Lab9/Webots/controllers/pythonController/exercise_9f.py
+++ Lab9/Webots/controllers/pythonController/exercise_9f.py
@@ -7,5 +7,24 @@ from simulation_parameters import SimulationParameters
 
 def exercise_9f(world, timestep, reset):
     """Exercise 9f"""
-    pass
+    mlr_drive = 2.  # Experiment for swimming only
+
+    parameter_set = [
+        SimulationParameters(
+            simulation_duration=30,
+            mlr_drive=mlr_drive,
+        )
+    ]
+
+    # Grid search
+    for simulation_i, parameters in enumerate(parameter_set):
+        reset.reset()
+        run_simulation(
+            world,
+            parameters,
+            timestep,
+            int(1000 * parameters.simulation_duration / timestep),
+            logs="./logs/9b_walking/simulation_{}.npz".format(simulation_i)
+        )
+
 
