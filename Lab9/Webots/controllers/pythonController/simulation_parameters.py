0b59c91601de14cfd7a69163f1dd420c3ee42afd
Loïc Niederhauser <loic.niederhauser@gmail.com>
2019-05-16 13:44:30 +0200

    Corrected simulation parameters in accordance with the paper

--- Lab9/Webots/controllers/pythonController/simulation_parameters.py
+++ Lab9/Webots/controllers/pythonController/simulation_parameters.py
@@ -34,10 +34,9 @@ class SimulationParameters(dict):
         self.v_sat = 0.
         self.R_sat = 0.
 
-        self.c_freq_body = [0.2, 0.3]  # Gave up on implementing default parameters. Useless in this scope anyway
-        self.c_freq_limb = [0.2, 0.0]
-        # self.c_R_body = [0.01, 0.1]
-        self.c_R_body = [0.065, 0.196]
+        self.c_freq_body = [0.3, 0.2]  # Gave up on implementing default parameters. Useless in this scope anyway
+        self.c_freq_limb = [0.0, 0.2]
+        self.c_R_body = [0.196, 0.065]
         self.c_R_limb = [0.131, 0.131]
 
         self.update(kwargs)  # NOTE: This overrides the previous declarations
